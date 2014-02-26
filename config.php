4ffc981a1a198dbeff2215082738e80e3938d782
Sandeep Gill <sandeep@lingellearning.com>
2014-02-26 14:56:42 +1100

    Config file reverted to original

--- config.php
+++ config.php
@@ -4,12 +4,27 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+defined('DB_USER')
+|| define('DB_USER', (getenv('DB_USER') ? getenv('DB_USER') : 'techsavv_moodle'));
+
+defined('DB_PASSWORD')
+|| define('DB_PASSWORD', (getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : 'T3chS@vvy!'));
+
+defined('DB_NAME')
+|| define('DB_NAME', (getenv('DB_NAME') ? getenv('DB_NAME') : 'techsavv_moodle2'));
+
+defined('DB_HOST')
+|| define('DB_HOST', (getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost'));
+
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'tech_savvy_deecd';
-$CFG->dbuser    = 'tech_savvy_user';
-$CFG->dbpass    = 'tech@123';
+$CFG->dbhost    = DB_HOST;
+$CFG->dbname    = DB_NAME;
+$CFG->dbuser    = DB_USER;
+$CFG->dbpass    = DB_PASSWORD;
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
@@ -17,8 +32,14 @@ $CFG->dboptions = array (
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = 'http://deecd.sandeepgill.com.au';
-$CFG->dataroot  = '/var/www/TECH_SAVVY/DEECD_DEV/moodledata';
+if (APPLICATION_ENV === 'production') {
+  $CFG->wwwroot   = 'http://deecd.tech-savvy.com.au';
+  $CFG->dataroot  = '/home/techsavv/subdomains/data/moodle-deecd';
+}
+else {
+  $CFG->wwwroot   = 'http://deecd.damiandennis.com';
+  $CFG->dataroot  = '/home/deecd/moodle-deecd';
+}
 
 $CFG->admin     = 'admin';
 
@@ -26,7 +47,9 @@ $CFG->directorypermissions = 0777;
 
 require_once(dirname(__FILE__) . '/lib/setup.php');
 
-$CFG->theme = 'mhpl';
+$CFG->lang=‘en’;
+$CFG->langcache=0;
+$CFG->langstringcache=0;
 
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
